044e34a3023b8bcfa122ec19d3f902ed47ff038f
zagcha <64097681+zagcha@users.noreply.github.com>
2021-04-05 19:23:26 +0300

    Create Kaz.js

---
+++ Kaz.js
@@ -0,0 +1,43 @@
+$("<div class='fr borderg' style='padding:2px;background-color:white;margin-right:4px;'>تصميم وبرمجه: <a href='https://chat-host.net/'>يوكام هوست</a></div>").insertAfter($(".loginstat"));  },1000);
+
+$(`<img src="https://chattty.com/uploads/ntsuursy6fvut6dw.jpeg" class="fr" style="width:100%;margin-right: 2px;">
+
+<a href="mm.html " style="width:90px;margin:2px;" class="fl mini fa fa-phone btn btn-warning"> اتصل بنا</a>
+<a href="iq.html" style="width:85px;margin:2px;" class="fl mini fa fa-heartbeat
+btn btn-danger"> ميز نفسك</a>
+<a href="rules.html" style="width:87px;margin:2px;" class="fl mini fa fa-star
+btn btn-success">قوانين</a>
+<a href="sup.html " style="width:90px;margin:2px;" class="fl mini fa fa-graduation-cap
+btn btn-info">الاشتراكات</a>
+
+
+
+
+
+`).insertBefore('.nav-tabs');
+
+
+
+$(".ustat").css("width",'12px').css('height','12px').css('border-radius','3em');
+
+$(".fa-gear").text("الاعدادات").css("width","");
+
+
+$('.u-msg.dots').removeClass('dots');
+
+$(document.getElementById("tbox").placeholder = "  　　　 حيـاكم شات بنوتة عشقي ");
+
+
+$('.u-msg.dots').removeClass('dots');
+
+
+
+
+$(".nav-tabs").addClass("fl").css("width","100%"),$("div#tlogins input").attr("maxlength",12),$("textarea#tbox.fl.corner.tbox.filw").attr("maxlength",100)
+
+
+
+
+
+
+vchat=true;
